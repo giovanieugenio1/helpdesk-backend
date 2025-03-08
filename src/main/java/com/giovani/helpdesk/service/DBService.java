edfdc8bfc4201c6090d455916e308d5fe82e7e8a
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2025-03-08 10:26:35 -0300

    adicionado mais registros para carga inicial

--- src/main/java/com/giovani/helpdesk/service/DBService.java
+++ src/main/java/com/giovani/helpdesk/service/DBService.java
@@ -29,36 +29,44 @@ public class DBService {
 
     @PostConstruct
     public void initDB() {
-        Tecnico tecnico = new Tecnico(
-                null,
-                "Giovani",
-                "12324543443",
-                "giovani@gmail.com",
-                "1234"
-        );
-        tecnico.addPerfil(Perfil.ADMIN);
+        Tecnico tecnico1 = new Tecnico(null, "Giovani", "12324543443", "giovani@gmail.com", "1234");
+        tecnico1.addPerfil(Perfil.ADMIN);
 
-        Cliente cliente = new Cliente(
-                null,
-                "John",
-                "21543675455",
-                "john@gmail.com",
-                "4321"
-        );
-        cliente.addPerfil(Perfil.CLIENTE);
+        Cliente cliente1 = new Cliente(null, "John", "21543675455", "john@gmail.com", "4321");
+        cliente1.addPerfil(Perfil.CLIENTE);
 
-        Chamado chamado = new Chamado(
-                null,
-                Prioridade.MEDIA,
-                Status.ABERTO,
-                "CH 1",
-                "Primeiro chamado",
-                tecnico,
-                cliente
-        );
+        Tecnico tecnico2 = new Tecnico(null, "Mariana", "32165498700", "mariana@gmail.com", "5678");
+        tecnico2.addPerfil(Perfil.TECNICO);
 
-        tecnicoRepository.saveAll(List.of(tecnico));
-        clienteRepository.saveAll(List.of(cliente));
-        chamadoRepository.saveAll(List.of(chamado));
+        Cliente cliente2 = new Cliente(null, "Carlos", "14785236912", "carlos@gmail.com", "8765");
+        cliente2.addPerfil(Perfil.CLIENTE);
+
+        Tecnico tecnico3 = new Tecnico(null, "Fernando", "96325874136", "fernando@gmail.com", "1357");
+        tecnico3.addPerfil(Perfil.ADMIN);
+
+        Cliente cliente3 = new Cliente(null, "Ana", "25874136985", "ana@gmail.com", "2468");
+        cliente3.addPerfil(Perfil.CLIENTE);
+
+        Tecnico tecnico4 = new Tecnico(null, "Ricardo", "75395185245", "ricardo@gmail.com", "9876");
+        tecnico4.addPerfil(Perfil.TECNICO);
+
+        Cliente cliente4 = new Cliente(null, "Beatriz", "95175325874", "beatriz@gmail.com", "3691");
+        cliente4.addPerfil(Perfil.CLIENTE);
+
+        Tecnico tecnico5 = new Tecnico(null, "Lucas", "85236974125", "lucas@gmail.com", "8520");
+        tecnico5.addPerfil(Perfil.ADMIN);
+
+        Cliente cliente5 = new Cliente(null, "Paula", "75362418579", "paula@gmail.com", "7410");
+        cliente5.addPerfil(Perfil.CLIENTE);
+
+        Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, Status.ABERTO, "CH 1", "Primeiro chamado", tecnico1, cliente1);
+        Chamado chamado2 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "CH 2", "Chamado urgente", tecnico2, cliente2);
+        Chamado chamado3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "CH 3", "Chamado resolvido", tecnico3, cliente3);
+        Chamado chamado4 = new Chamado(null, Prioridade.MEDIA, Status.ABERTO, "CH 4", "Outro chamado", tecnico4, cliente4);
+        Chamado chamado5 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "CH 5", "Suporte avançado", tecnico5, cliente5);
+
+        tecnicoRepository.saveAll(List.of(tecnico1, tecnico2, tecnico3, tecnico4, tecnico5));
+        clienteRepository.saveAll(List.of(cliente1, cliente2, cliente3, cliente4, cliente5));
+        chamadoRepository.saveAll(List.of(chamado1, chamado2, chamado3, chamado4, chamado5));
     }
 }
