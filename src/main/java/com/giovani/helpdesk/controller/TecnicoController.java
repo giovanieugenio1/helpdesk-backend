f0994a41bee80d8c693fb3683a061280af2319b3
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2025-03-08 10:27:33 -0300

    criado endpoint getTecnicoDetails

---
+++ src/main/java/com/giovani/helpdesk/controller/TecnicoController.java
@@ -0,0 +1,26 @@
+package com.giovani.helpdesk.controller;
+
+import com.giovani.helpdesk.domain.Tecnico;
+import com.giovani.helpdesk.service.TecnicoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/tecnicos")
+@RestController
+public class TecnicoController {
+
+    private final TecnicoService tecnicoService;
+
+    public TecnicoController(TecnicoService tecnicoService) {
+        this.tecnicoService = tecnicoService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Tecnico> getTecnicoDetails(@PathVariable("id") Integer id) {
+        var tecnico = tecnicoService.getTecnicoDetails(id);
+        return ResponseEntity.ok().body(tecnico);
+    }
+}
